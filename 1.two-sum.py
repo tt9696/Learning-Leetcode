7613692e0731054621e950440f0a68a65f7c7598
tt9696 <taysc96@gmail.com>
2024-07-18 16:06:33 +0800

    Solve

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -12,6 +12,37 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        result = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+        return result
+    
+
+    """
+    num_index = {}
+
+    for index, num in enumerate(nums):
+        complement = target - num
+
+        if complement in num_index:
+            return [num_index[complement], index]
+
+        num_index[num] = index
+
+    Iteration 1:
+    index = 0, num = 2
+    complement = 9 - 2 = 7
+    7 is not in num_to_index
+    Update num_to_index to {2: 0}
+    Iteration 2:
+
+    index = 1, num = 7
+    complement = 9 - 7 = 2
+    2 is in num_to_index (it's the number from the previous iteration)
+    Return indices [0, 1]
+    """
 # @lc code=end
 
